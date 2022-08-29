27c2f3316a8e0b8cdb1f38ab81846624a3bfb7b8
Masih <haashemim@gmail.com>
2022-08-28 22:04:59 -0400

    "Commented Grid World"

--- GridEnv.py
+++ GridEnv.py
@@ -5,6 +5,8 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+# Dict Of Actions
 ALL_ACTIONS = {
     'U': 0,
     'D': 1,
@@ -17,7 +19,9 @@ ALL_ACTIONS = {
     'DR': 9
 }
 
-
+''' Gets action by name, and x y postion
+    Return updated postion
+'''
 def move(action_name, x, y):
     assert action_name in ALL_ACTIONS.keys()
 
@@ -57,13 +61,14 @@ class WindyGridworld(Env):
         )
 
         self.actions = allowed_actions
-        # Action Space attr
+
+        # Action Space attr required by gym
         self.action_space = Discrete(len(allowed_actions))
 
-        # observation space
         self.canvas = None
-        self.observation_shape = (84, 84, 3)
 
+        # observation space and shape required by gym
+        self.observation_shape = (84, 84, 3)
         self.observation_space = spaces.Box(low=0, high=255, shape=(84, 84, 3), dtype=np.uint8)
 
 
@@ -75,16 +80,23 @@ class WindyGridworld(Env):
         else:
             return self.__pos
 
+    '''Takes an integer input which represents an action.
+    Returns observation image, Reward, and terminal state marker as required by gym
+    '''
     def step(self, action):
 
         x, y = self.pos
 
+        # Check action 
         assert action in range(self.action_space.n)
+        # Move
         x, y = move(self.actions[action], x, y)
 
+        # Add wind
         if self.wind:
             x -= 1
 
+        # Calculate Reward
         reward = 0
         for i, reward_spec in enumerate(self.rewards):
             value, x_, y_ = reward_spec
@@ -106,7 +118,9 @@ class WindyGridworld(Env):
         self.canvas = visualize(self)
 
         return self.canvas, reward, done, {}
-
+    
+    '''Get player position and check to see if player has hit boundry of grid.
+    '''
     def check_terminal_state(self, x, y):
         if x < 0: return True
         if y < 0: return True
@@ -115,6 +129,8 @@ class WindyGridworld(Env):
 
         return False
 
+    '''Reset the gym env. Return observation as required by gym
+    '''
     def reset(self):
 
         if self.start == 'random':
@@ -145,7 +161,8 @@ class WindyGridworld(Env):
     def close(self):
         cv2.destroyAllWindows()
 
-
+'''Get WindyGridWorld Object as input, 
+return (84, 84, 3) image representation of gridworld'''
 def visualize(env, black_bar=True):
     # Colors
     rgb_colors = {}
@@ -156,6 +173,7 @@ def visualize(env, black_bar=True):
     # Fill all but border
     canvas[1:-1, 1:-1, :] = 0
 
+    # Draw reward
     for i, reward in enumerate(env.rewards):
         if env.claimed[i] != 0:
             value, x_, y_ = reward
@@ -169,12 +187,14 @@ def visualize(env, black_bar=True):
                 canvas[x_ + 1, y_ + 1, i] = c[i]
                 i += 1
 
+    # Draw player
     x, y = env.pos
     i = 0
     while i < 3:
         canvas[x + 1, y + 1, i] = rgb_colors['yellow'][i]
         i += 1
 
+    # Add blackbar and make image square to keep aspect ratio of original grid world
     if black_bar:
         delta = canvas.shape[0] - canvas.shape[1]
         black_bar = np.zeros((env.height + 2, delta // 2))
@@ -183,6 +203,7 @@ def visualize(env, black_bar=True):
         c = np.column_stack((black_bar, canvas[:, :, 2], black_bar))
         canvas = np.stack([a, b, c], axis=2)
 
+    # Resive to 84 x 84
     r = cv2.resize(canvas[:, :, 0], (84, 84), interpolation=cv2.INTER_NEAREST)
     g = cv2.resize(canvas[:, :, 1], (84, 84), interpolation=cv2.INTER_NEAREST)
     b = cv2.resize(canvas[:, :, 2], (84, 84), interpolation=cv2.INTER_NEAREST)
